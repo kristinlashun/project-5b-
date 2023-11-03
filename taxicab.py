b7869e9b33fbc2c4b8cbb4fac8da6e911821867d
Kristin Evans <kristinlashun@gmail.com>
2023-11-02 20:25:53 -0700

    cases

--- taxicab.py
+++ taxicab.py
@@ -1,6 +1,31 @@
-def multiply(a, b):
-    if b == 1:
-        return a
+class Taxicab:
     
-    else:
-        return a + multiply(a, b-1)
+
+    def __init__(self, x, y):
+        
+        self.__x = x
+        self.__y = y
+        self.__odometer = 0
+    
+    def get_x_coord(self):
+        
+        return self.__x
+    
+    def get_y_coord(self):
+        
+        return self.__y
+    
+    def get_odometer(self):
+        
+        return self.__odometer
+    
+    def move_x(self, dx):
+        
+        self.__x += dx
+        self.__odometer += abs(dx)
+    
+    def move_y(self, dy):
+      
+        self.__y += dy
+        self.__odometer += abs(dy)
+
